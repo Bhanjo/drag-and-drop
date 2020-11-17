d5c099ca616f4457830b07b25b2039f9183a1efd
Bhanjo <hanjo1515@naver.com>
2020-11-17 13:57:44 +0900

    20_11_17

--- dragEvent.js
+++ dragEvent.js
@@ -1,43 +1,16 @@
-// function allowDrop() {
-//     Event.preventDefault();
-// }
-
-// function dropItem() {
-//     var targetEle = event.target;
-//     var id = Evnet.dataTransfer.getData('text');
-//     var moveEle = document.getElementById(id);
-//     targetEle.before(moveEle);
-// }
-
-// function dragStart() {
-//     var thisEle = Event.target;
-//     var thisId = thisEle.id;
-//     // thisEle.classList.add('is-dragging');
-//     Event.dataTransfer.setData('text/plain', thisId);
-// }
-
-// function dragEnd() {
-//     var thisEle = Event.target;
-//     // thisEle.classList.remove('is-dragging');
-// }
-
 function allowDrop() {
     event.preventDefault();
-  }
-  
+}
+
 function dropItem() {
-    var _targetEle = event.target;
-    var _id = event.dataTransfer.getData('text');
-    var _moveEle = document.getElementById(_id );
-    _targetEle.appendChild(_moveEle);
-  }
-  
-  function dragStart() {
-    var _thisEle = event.target;
-    var _thisId = _thisEle.id;
-    event.dataTransfer.setData('text/plain', _thisId);
-  }
-  
-  function dragEnd() {
-    var _thisEle = event.target;
-  }
+    var dropele = event.target;
+    var dropid = event.dataTransfer.getData('text');
+    var moveEle = document.getElementById(dropid);
+    dropele.appendChild(moveEle);
+}
+
+function dragStart() {
+    var selectEle = event.target;
+    var selectId = selectEle.id;
+    event.dataTransfer.setData('text', selectId);
+}
